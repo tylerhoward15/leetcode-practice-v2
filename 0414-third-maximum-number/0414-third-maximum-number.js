fbde81003e2b467ef4b64fb53fbf8c0147662ca2
Tyler Howard <Tyler98ky@gmail.com>
2023-10-11 01:06:14 -0700

    Time: 52 ms (73.38%), Space: 44 MB (59.88%) - LeetHub

---
+++ 0414-third-maximum-number/0414-third-maximum-number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var thirdMax = function(nums) {
+    const temp = nums.sort((a, b) => {return a-b}).reverse().slice()
+    console.log('temp', temp)
+    let count = 1
+    let slow = 0
+    for (let i = 1; i < temp.length; i++) {
+        if (temp[slow] != temp[i]) {
+            console.log(slow,i,count)
+            count++;
+            slow = i
+            if (count == 3) {
+                return temp[slow]
+            }
+        }
+    }
+
+    return temp[0];
+};
