f731b083451f478632e8e62fe27b6cc93cd85077
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-14 18:22:38 -0700

    Time: 69 ms (54.92%), Space: 49.4 MB (22.51%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.js
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.js
@@ -14,11 +14,8 @@
 var searchBST = function(root, val) {
     if (root == null) {
         return null
-    }
-
-    if (root.val < val) {
+    } else if (root.val < val) {
         return searchBST(root.right, val)
-
     } else if (root.val > val) {
         return searchBST(root.left, val)
     } else {
