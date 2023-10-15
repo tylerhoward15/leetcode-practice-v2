01aa09a5ad9c17839421d1edaf18a68fedb6ddf4
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-15 15:24:44 -0700

    Time: 174 ms (5.05%), Space: 53.2 MB (5.22%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    const mag = new Map()
+
+    Array.from(magazine).forEach(letter => {
+        mag.set(letter, getOrDefault(mag, letter)+1)
+    })
+
+    return Array.from(ransomNote).every(letter => {
+        console.log(mag)
+        const temp = mag.get(letter)
+        if (temp > 0) {
+            mag.set(letter, temp-1)
+            return true
+        } else {
+            return false
+        }
+    });
+};
+
+function getOrDefault(map, letter) {
+    const res = map.get(letter)
+    return res == null ? 0 : res;
+}
