7a3e8a8b8dd731424f3da28c7b3ad8c7ab648874
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-18 13:12:51 -0700

    Time: 74 ms (92.69%), Space: 70.4 MB (38.4%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.js
@@ -0,0 +1,17 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var countNodes = function(root) {
+    if (root == null) return 0;
+
+    return 1 + countNodes(root.left) + countNodes(root.right)
+};
