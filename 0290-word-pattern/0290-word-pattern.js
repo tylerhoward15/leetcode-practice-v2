6120c577219b0957828ad5c67b236cefdee5ee99
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-19 17:37:18 -0700

    Time: 52 ms (48.27%), Space: 41.5 MB (87.38%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.js
+++ 0290-word-pattern/0290-word-pattern.js
@@ -4,15 +4,13 @@
  * @return {boolean}
  */
 var wordPattern = function(pattern, s) {
-    const map = new Map()
-    const used = new Set()
     const chars = pattern.split('')
     const words = s.split(" ")
-
     if (chars.length != words.length) return false;
-    // console.log(`words or s`, words)
-    // console.log(`chars or pattern`, chars)
 
+    const map = new Map()
+    const used = new Set()
+    
     const bool = words.every((word, index) => {
         if (!map.has(word)) {
             if (used.has(chars[index])) {
