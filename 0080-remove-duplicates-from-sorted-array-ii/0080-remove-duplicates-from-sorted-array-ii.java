663a0655e9a49315b039deee147011030c3e9801
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-24 18:27:28 -0700

    Time: 6 ms (5%), Space: 43.5 MB (64.33%) - LeetHub

---
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int ans = 0;
+        Map<Integer, Integer> freqs = new HashMap<>(); // k: number, v: occurences
+
+        for (int i=nums.length-1; i>=0; i--) {
+            final int freq = freqs.getOrDefault(nums[i], 0);
+            if (freq < 2) {
+                freqs.put(nums[i], freq+1);
+            } else {
+                // if (nums[i] == nums[0]) break;
+                removeElem(nums,i);
+                ans++;
+                // i++;
+            }
+        }
+
+
+        System.out.println(Arrays.toString(nums));
+        return nums.length - ans;
+    }
+
+    public void removeElem(int[] nums, int index) {
+        for (int i=index+1; i<nums.length;i++){
+            nums[i-1] = nums[i];
+        }
+    }
+}
