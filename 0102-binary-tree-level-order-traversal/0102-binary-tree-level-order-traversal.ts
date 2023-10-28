1a8d4e7f57ecc2492ba81db315680a8f52e984a0
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-10-27 22:50:08 -0700

    Time: 78 ms (29.1%), Space: 46.6 MB (92.6%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.ts
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function levelOrder(root: TreeNode | null): number[][] {
+    const q: TreeNode[] = [];
+    const ret: number[][] = [];
+    q.push(root);
+
+    while(q.length != 0) {
+        const level: number[] = [];
+        const size: number = q.length;
+
+        for(let i: number = 0; i<size; i++) {
+            const current: TreeNode = q.shift();
+            if (current != null) {
+                level.push(current.val);
+                if (current.left != null) {
+                    q.push(current.left);
+                }
+                if (current.right != null) {
+                    q.push(current.right);
+                }
+            }
+        }
+        if (level.length != 0) {
+            ret.push(level);
+        }
+    }
+    return ret;
+};
