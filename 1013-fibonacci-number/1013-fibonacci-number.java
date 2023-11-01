0234cf18ceed7643f07587cfbe70d3a9f789b91d
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-11-01 10:06:06 -0700

    Time: 0 ms (100%), Space: 39.1 MB (42.84%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -1,24 +1,16 @@
 class Solution {
     public int fib(int n) {
         Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 0);
-        map.put(1, 1);
-        map.put(2, 1);
-        map.put(3,2);
-        map.put(4, 3);
-
-
-        fib(map, 0);
+        map.put(0,0);
+        map.put(1,1);
+        
         return fib(map, n);
     }
 
-    public int fib(Map<Integer, Integer> fibs, int n) {
-        final int curr = fibs.getOrDefault(n, -1);
-
-        if(curr == -1) {
-            return fib(fibs, n-2) + fib(fibs, n-1);
+    public int fib(Map<Integer, Integer> memo, int n) {
+        if (!memo.containsKey(n)) {
+            memo.put(n, fib(memo, n-1) + fib(memo, n-2));
         }
-
-        return curr;
+        return memo.get(n);
     }
 }
