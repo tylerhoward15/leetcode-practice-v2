ac1bf9f4a3d54485c6b86aac8e64bc1b826233ce
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-11-05 22:00:50 -0800

    Time: 52 ms (6.85%), Space: 44.2 MB (6.52%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String intToRoman(int num) {
+        System.out.println(num);
+        if (num <= 0) {
+            return "";
+        }
+        if (num >= 1000) {
+            return "M" + intToRoman(num-1000);
+        }
+        if (num >= 500) {
+            if (num >= 900) {
+                return "CM" + intToRoman(num-900);
+            }
+            return "D" + intToRoman(num-500);
+        }
+        if (num >= 100) {
+            if (num >= 400) {
+                return "CD" + intToRoman(num-400);
+            }
+            return "C" + intToRoman(num-100);
+        }
+        if (num >= 50) {
+            if (num >= 90) {
+                return "XC" + intToRoman(num-90);
+            }
+            return "L" + intToRoman(num-50);
+        }
+        if (num >= 10) {
+            if (num >= 40) {
+                return "XL" + intToRoman(num-40);
+            }
+            return "X" + intToRoman(num-10);
+        }
+        if (num >= 5) {
+            if (num >= 9) {
+                return "IX" + intToRoman(num-9);
+            }
+            return "V" + intToRoman(num-5);
+        }
+
+        if (num == 4) {
+            return "IV" + intToRoman(num-4);
+        }
+        return "I" + intToRoman(num-1);
+    }
+}
