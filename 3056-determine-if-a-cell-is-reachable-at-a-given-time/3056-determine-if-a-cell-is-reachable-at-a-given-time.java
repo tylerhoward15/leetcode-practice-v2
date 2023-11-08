5c808c3207ea25330bf9b152719c267c8d9aaa5a
Tyler Howard <22920537+Tyler98ky@users.noreply.github.com>
2023-11-08 08:11:24 -0800

    Time: 1 ms (95.3%), Space: 39.7 MB (62.62%) - LeetHub

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.java
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/3056-determine-if-a-cell-is-reachable-at-a-given-time.java
@@ -1,14 +1,6 @@
 class Solution {
     public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
         if (sx == fx && sy == fy && t==1) {return false;}
-        return t >= cheby(sx,sy,fx,fy);
+        return t >= Math.max(Math.abs(sx-fx),Math.abs(sy-fy));
     }
-
-    public long cheby(int sx, int sy, int fx, int fy) {
-        int x = Math.abs(sx-fx);
-        int y = Math.abs(sy-fy);
-        System.out.println(Math.max(x,y));
-        return Math.max(x,y);
-    }
-
 }
